a3992c35806100242d28388afc4e6ae507e23246
Jesparza17 <jesparza@tulane.edu>
2021-08-26 10:29:16 -0500

    Lab 1 Tutorial
    Lab 1 recitation

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def sum_of_squares(a):
-	pass
+  sum = 0
+  for number in a :
+    square = number**2
+    sum +=square
+  return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+    assert sum_of_squares([2,3,4]) == 25
+
+test_one()
+test_two()
